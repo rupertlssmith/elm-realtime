e325f01f86e5b58e683a43d094f04f62145d797f
Rupert Smith <rupert@thesett.com>
2024-12-09 13:50:03 +0000

    First pass at defining the realtime module with a stateless API.

--- packages/shared/src/elm/Realtime.elm
+++ packages/shared/src/elm/Realtime.elm
@@ -1,10 +1,6 @@
 module Realtime exposing
     ( Model
-    , Msg
-    , Protocol
     , init
-    , subscriptions
-    , update
     )
 
 import Http
@@ -13,33 +9,38 @@ import Json.Decode.Extra as DE
 import Json.Encode as Encode exposing (Value)
 import Momento exposing (Error, MomentoSessionKey, OpenParams, SubscribeParams)
 import Ports
+import Procedure exposing (Procedure)
+import Procedure.Channel as Channel
 import Procedure.Program
 import Random
-import Update2 as U2
-
-
-type alias Realtime a =
-    { a
-        | rtChannelApiUrl : String
-        , momentoApiKey : String
-        , app : Model
-    }
 
 
 type alias Model =
-    { procedure : Procedure.Program.Model Msg
-    , lifecycle : Lifecycle
+    { rtChannelApiUrl : String
+    , momentoApiKey : String
+    , state : State
     }
 
 
-type Msg
-    = ProcedureMsg (Procedure.Program.Msg Msg)
-    | RandomSeed Random.Seed
-    | ChannelDetails (Result Http.Error Channel)
-    | MMOpened (Result Error MomentoSessionKey)
-    | MMSubscribed (Result Error MomentoSessionKey)
-    | MMNotified (Result Error MomentoSessionKey)
-    | MMOnMessage MomentoSessionKey Value
+type alias Delta =
+    Model -> Model
+
+
+type Error
+    = HttpError Http.Error
+    | MomentoError Momento.Error
+
+
+type State
+    = StartState
+    | RunningState RunningProps
+
+
+type alias RunningProps =
+    { sessionKey : MomentoSessionKey
+    , seed : Random.Seed
+    , channel : Channel
+    }
 
 
 type alias Channel =
@@ -49,255 +50,6 @@ type alias Channel =
     , saveList : String
     , webhook : String
     }
-
-
-type Lifecycle
-    = ModelStart StartState
-    | ModelRandomized RandomizedState
-    | ModelChannelDetails ChannelDetailsState
-    | ModelConnected ConnectedState
-    | ModelRunning RunningState
-
-
-type alias StartState =
-    { log : List String
-    }
-
-
-type alias RandomizedState =
-    { log : List String
-    , seed : Random.Seed
-    }
-
-
-type alias ChannelDetailsState =
-    { log : List String
-    , seed : Random.Seed
-    , channel : Channel
-    }
-
-
-type alias ConnectedState =
-    { log : List String
-    , socketHandle : MomentoSessionKey
-    , seed : Random.Seed
-    , channel : Channel
-    }
-
-
-type alias RunningState =
-    { log : List String
-    , socketHandle : MomentoSessionKey
-    , seed : Random.Seed
-    , channel : Channel
-    }
-
-
-setModel m x =
-    { m | app = x }
-
-
-setLifecycle m x =
-    { m | lifecycle = x }
-
-
-switchState : (a -> Lifecycle) -> a -> ( Lifecycle, Cmd Msg )
-switchState cons state =
-    ( cons state
-    , Cmd.none
-    )
-
-
-type alias Protocol submodel msg model =
-    { toMsg : Msg -> msg
-    , onUpdate : ( submodel, Cmd msg ) -> ( model, Cmd msg )
-    }
-
-
-momentoApi : Momento.MomentoApi Msg
-momentoApi =
-    { open = Ports.mmOpen
-    , close = Ports.mmClose
-    , subscribe = Ports.mmSubscribe
-    , publish = Ports.mmPublish
-    , onMessage = Ports.mmOnMessage
-    , pushList = Ports.mmPushList
-    , popList = Ports.mmPopList
-    , createWebhook = Ports.mmCreateWebhook
-    , response = Ports.mmResponse
-    , asyncError = Ports.mmAsyncError
-    }
-        |> Momento.momentoApi ProcedureMsg
-
-
-init : (Msg -> msg) -> ( Model, Cmd msg )
-init toMsg =
-    { log = [ "Started" ]
-    }
-        |> U2.pure
-        |> U2.andMap randomize
-        |> U2.andMap (switchState ModelStart)
-        |> Tuple.mapFirst
-            (\state ->
-                { procedure = Procedure.Program.init
-                , lifecycle = state
-                }
-            )
-        |> Tuple.mapSecond (Cmd.map toMsg)
-
-
-subscriptions : Protocol (Realtime a) msg model -> Realtime a -> Sub msg
-subscriptions protocol component =
-    let
-        model =
-            component.app
-    in
-    [ Procedure.Program.subscriptions model.procedure
-    , momentoApi.onMessage MMOnMessage
-    ]
-        |> Sub.batch
-        |> Sub.map protocol.toMsg
-
-
-update : Protocol (Realtime a) msg model -> Msg -> Realtime a -> ( model, Cmd msg )
-update protocol msg component =
-    let
-        model =
-            component.app
-
-        lifecycle =
-            model.lifecycle
-    in
-    case ( lifecycle, msg |> Debug.log "update" ) of
-        ( _, ProcedureMsg innerMsg ) ->
-            let
-                ( procMdl, procMsg ) =
-                    Procedure.Program.update innerMsg model.procedure
-            in
-            ( { model | procedure = procMdl }, procMsg )
-                |> Tuple.mapFirst (setModel component)
-                |> Tuple.mapSecond (Cmd.map protocol.toMsg)
-                |> protocol.onUpdate
-
-        ( ModelStart state, RandomSeed seed ) ->
-            { log = "Randomized" :: state.log
-            , seed = seed
-            }
-                |> U2.pure
-                |> U2.andThen (getChannelDetails component)
-                |> U2.andMap (switchState ModelRandomized)
-                |> Tuple.mapFirst (setLifecycle model)
-                |> Tuple.mapFirst (setModel component)
-                |> Tuple.mapSecond (Cmd.map protocol.toMsg)
-                |> protocol.onUpdate
-
-        ( ModelRandomized state, ChannelDetails (Ok channel) ) ->
-            ( { log = "ChannelDetails" :: state.log
-              , seed = state.seed
-              , channel = channel
-              }
-            , momentoApi.open
-                { apiKey = component.momentoApiKey
-                , cache = cacheName
-                }
-                MMOpened
-            )
-                |> U2.andMap (switchState ModelChannelDetails)
-                |> Tuple.mapFirst (setLifecycle model)
-                |> Tuple.mapFirst (setModel component)
-                |> Tuple.mapSecond (Cmd.map protocol.toMsg)
-                |> protocol.onUpdate
-
-        ( ModelChannelDetails state, MMOpened (Ok sessionKey) ) ->
-            ( { log = "Connected" :: state.log
-              , seed = state.seed
-              , channel = state.channel
-              , socketHandle = sessionKey
-              }
-            , momentoApi.subscribe
-                sessionKey
-                { topic = state.channel.modelTopic }
-                MMSubscribed
-            )
-                |> U2.andMap (switchState ModelConnected)
-                |> Tuple.mapFirst (setLifecycle model)
-                |> Tuple.mapFirst (setModel component)
-                |> Tuple.mapSecond (Cmd.map protocol.toMsg)
-                |> protocol.onUpdate
-
-        ( ModelConnected state, MMSubscribed (Ok sessionKey) ) ->
-            let
-                payload =
-                    [ ( "message", Encode.string "hello" )
-                    ]
-                        |> Encode.object
-
-                notice =
-                    [ ( "rt", Encode.string "N" )
-                    , ( "client", Encode.string "abcdef" )
-                    ]
-                        |> Encode.object
-
-                transient =
-                    [ ( "rt", Encode.string "T" )
-                    , ( "client", Encode.string "abcdef" )
-                    , ( "payload", payload )
-                    ]
-                        |> Encode.object
-
-                unsaved =
-                    [ ( "rt", Encode.string "U" )
-                    , ( "client", Encode.string "abcdef" )
-                    , ( "payload", payload )
-                    ]
-                        |> Encode.object
-            in
-            ( { log =
-                    ("PushList: " ++ Encode.encode 2 payload)
-                        :: ("Publish: " ++ Encode.encode 2 notice)
-                        :: ("Subscribed: " ++ state.channel.modelTopic)
-                        :: state.log
-              , seed = state.seed
-              , channel = state.channel
-              , socketHandle = sessionKey
-              }
-            , Cmd.batch
-                [ momentoApi.publish sessionKey
-                    { topic = state.channel.saveTopic, payload = notice }
-                    MMNotified
-                , momentoApi.pushList sessionKey
-                    { list = state.channel.saveList, payload = unsaved }
-                    MMNotified
-                , momentoApi.publish sessionKey
-                    { topic = state.channel.modelTopic, payload = transient }
-                    MMNotified
-                ]
-            )
-                |> U2.andMap (switchState ModelRunning)
-                |> Tuple.mapFirst (setLifecycle model)
-                |> Tuple.mapFirst (setModel component)
-                |> Tuple.mapSecond (Cmd.map protocol.toMsg)
-                |> protocol.onUpdate
-
-        ( ModelRunning state, MMOnMessage _ payload ) ->
-            { state | log = ("Message: " ++ String.slice 0 90 (Encode.encode 2 payload) ++ "...") :: state.log }
-                |> U2.pure
-                |> U2.andMap (switchState ModelRunning)
-                |> Tuple.mapFirst (setLifecycle model)
-                |> Tuple.mapFirst (setModel component)
-                |> Tuple.mapSecond (Cmd.map protocol.toMsg)
-                |> protocol.onUpdate
-
-        _ ->
-            U2.pure component
-                |> protocol.onUpdate
-
-
-randomize : StartState -> ( StartState, Cmd Msg )
-randomize model =
-    ( model
-    , Random.generate RandomSeed Random.independentSeed
-    )
 
 
 channelDecoder : Decoder Channel
@@ -310,16 +62,265 @@ channelDecoder =
         |> DE.andMap (Decode.field "webhook" Decode.string)
 
 
-getChannelDetails : Realtime a -> RandomizedState -> ( RandomizedState, Cmd Msg )
-getChannelDetails component state =
-    ( state
-    , Http.get
-        { url = component.rtChannelApiUrl ++ "/v1/channel"
-        , expect = Http.expectJson ChannelDetails channelDecoder
+buildMomentoApi : (Procedure.Program.Msg msg -> msg) -> Momento.MomentoApi msg
+buildMomentoApi pt =
+    { open = Ports.mmOpen
+    , close = Ports.mmClose
+    , subscribe = Ports.mmSubscribe
+    , publish = Ports.mmPublish
+    , onMessage = Ports.mmOnMessage
+    , pushList = Ports.mmPushList
+    , popList = Ports.mmPopList
+    , createWebhook = Ports.mmCreateWebhook
+    , response = Ports.mmResponse
+    , asyncError = Ports.mmAsyncError
+    }
+        |> Momento.momentoApi pt
+
+
+
+-- Initialization procedure.
+
+
+{-| Initializes the realtime channel:
+
+    * Fetches the channel details from the Channel API.
+    * Opens a connection to Momento.
+    * Subscribes to the model topic for the channel.
+
+-}
+init :
+    (Procedure.Program.Msg msg -> msg)
+    ->
+        { rtChannelApiUrl : String
+        , momentoApiKey : String
         }
+    -> (Result Error Model -> msg)
+    -> Cmd msg
+init pt props rt =
+    let
+        momentoApi =
+            buildMomentoApi pt
+
+        model =
+            { rtChannelApiUrl = props.rtChannelApiUrl
+            , momentoApiKey = props.momentoApiKey
+            , state = StartState
+            }
+
+        initProcedure =
+            randomize
+                |> Procedure.andThen (getChannelDetails model)
+                |> Procedure.andThen (openMomentoCache model momentoApi)
+                |> Procedure.andThen (subscribeModelTopic momentoApi)
+                |> Procedure.map
+                    (\state ->
+                        { rtChannelApiUrl = props.rtChannelApiUrl
+                        , momentoApiKey = props.momentoApiKey
+                        , state = RunningState state
+                        }
+                    )
+    in
+    initProcedure
+        |> Procedure.try pt rt
+
+
+randomize : Procedure e Random.Seed msg
+randomize =
+    (\rt -> Random.generate rt Random.independentSeed)
+        |> Procedure.fetch
+
+
+getChannelDetails :
+    Model
+    -> Random.Seed
+    ->
+        Procedure Error
+            { seed : Random.Seed
+            , channel : Channel
+            }
+            msg
+getChannelDetails model seed =
+    (\rt ->
+        Http.get
+            { url = model.rtChannelApiUrl ++ "/v1/channel"
+            , expect = Http.expectJson rt channelDecoder
+            }
     )
+        |> Procedure.fetch
+        |> Procedure.andThen
+            (\getResult ->
+                case getResult of
+                    Ok channel ->
+                        Procedure.provide channel
+
+                    Err err ->
+                        HttpError err |> Procedure.break
+            )
+        |> Procedure.map (\channel -> { seed = seed, channel = channel })
 
 
-cacheName : String
-cacheName =
+openMomentoCache :
+    Model
+    -> Momento.MomentoApi msg
+    ->
+        { seed : Random.Seed
+        , channel : Channel
+        }
+    -> Procedure.Procedure Error RunningProps msg
+openMomentoCache model momentoApi state =
+    momentoApi.open
+        { apiKey = model.momentoApiKey
+        , cache = cacheName state.channel.id
+        }
+        |> Procedure.fetchResult
+        |> Procedure.mapError MomentoError
+        |> Procedure.map
+            (\sessionKey ->
+                { seed = state.seed
+                , channel = state.channel
+                , sessionKey = sessionKey
+                }
+            )
+
+
+subscribeModelTopic :
+    Momento.MomentoApi msg
+    -> RunningProps
+    -> Procedure.Procedure Error RunningProps msg
+subscribeModelTopic momentoApi state =
+    momentoApi.subscribe
+        state.sessionKey
+        { topic = state.channel.modelTopic }
+        |> Procedure.fetchResult
+        |> Procedure.mapError MomentoError
+        |> Procedure.map
+            (\sessionKey -> { state | sessionKey = sessionKey })
+
+
+
+-- Send realtime messages on the channel.
+
+
+{-| Sends a persisted message. This message will be assigned a unique and contiguous sequence number and
+saved into an event log, before being forwarded to all clients on the same channel.
+-}
+publishPersisted :
+    (Procedure.Program.Msg msg -> msg)
+    -> Model
+    -> Value
+    -> (Result Error Delta -> msg)
+    -> Cmd msg
+publishPersisted pt model payload rt =
+    let
+        momentoApi =
+            buildMomentoApi pt
+    in
+    case model.state of
+        RunningState state ->
+            let
+                notice =
+                    [ ( "rt", Encode.string "N" )
+                    , ( "client", Encode.string "abcdef" )
+                    ]
+                        |> Encode.object
+
+                unsaved =
+                    [ ( "rt", Encode.string "U" )
+                    , ( "client", Encode.string "abcdef" )
+                    , ( "payload", payload )
+                    ]
+                        |> Encode.object
+            in
+            momentoApi.publish state.sessionKey { topic = state.channel.saveTopic, payload = notice }
+                |> Procedure.fetchResult
+                |> Procedure.andThen
+                    (\sk ->
+                        momentoApi.pushList sk
+                            { list = state.channel.saveList, payload = unsaved }
+                            |> Procedure.fetchResult
+                    )
+                |> Procedure.map
+                    (\sk mdl ->
+                        case mdl.state of
+                            RunningState innerState ->
+                                { mdl | state = { innerState | sessionKey = sk } |> RunningState }
+
+                            _ ->
+                                mdl
+                    )
+                |> Procedure.mapError MomentoError
+                |> Procedure.try pt rt
+
+        _ ->
+            Cmd.none
+
+
+{-| Sends a transient message. This message will be forwarded to all clients on the same channel, but will
+not be saved.
+-}
+publishTransient :
+    (Procedure.Program.Msg msg -> msg)
+    -> Model
+    -> Value
+    -> (Result Error Delta -> msg)
+    -> Cmd msg
+publishTransient pt model payload rt =
+    let
+        momentoApi =
+            buildMomentoApi pt
+    in
+    case model.state of
+        RunningState state ->
+            let
+                transient =
+                    [ ( "rt", Encode.string "T" )
+                    , ( "client", Encode.string "abcdef" )
+                    , ( "payload", payload )
+                    ]
+                        |> Encode.object
+            in
+            momentoApi.publish state.sessionKey { topic = state.channel.saveTopic, payload = transient }
+                |> Procedure.fetchResult
+                |> Procedure.map
+                    (\sk mdl ->
+                        case mdl.state of
+                            RunningState innerState ->
+                                { mdl | state = { innerState | sessionKey = sk } |> RunningState }
+
+                            _ ->
+                                mdl
+                    )
+                |> Procedure.mapError MomentoError
+                |> Procedure.try pt rt
+
+        _ ->
+            Cmd.none
+
+
+
+-- Receive messages from the channel.
+
+
+onMessage :
+    (Procedure.Program.Msg msg -> msg)
+    -> Model
+    -> (Value -> msg)
+    -> Sub msg
+onMessage pt model rt =
+    let
+        momentoApi =
+            buildMomentoApi pt
+    in
+    case model.state of
+        RunningState state ->
+            (\val -> momentoApi.onMessage (always val))
+                rt
+
+        _ ->
+            Sub.none
+
+
+cacheName : String -> String
+cacheName _ =
     "elm-realtime-cache"
